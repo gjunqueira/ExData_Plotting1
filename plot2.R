02dd9c8eeec8a7f692e63b5196e1805713f94377
gjunqueira <gjunqueira@gmail.com>
2016-04-16 22:23:30 -0300

    Halfway there

---
+++ plot2.R
@@ -0,0 +1,20 @@
+# Needed libraries
+require(dplyr)
+
+# Reading data and discarding unwanted data by subseting
+data <- read.csv("./household_power_consumption.txt", na.strings = "?", sep = ";")
+
+# Discarding unwanted data by subseting
+data       <- filter(data, Date == "1/2/2007" | Date == "2/2/2007")
+
+# Using data from two columns to generate a full date
+fullDate   <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+
+# Keeping the full date and discarding the two other columns
+data       <- select(cbind(fullDate,data), -c(2,3))
+
+png(filename="./plot2.png", width=480, height=480, units="px")
+
+with(data, plot(x=fullDate, y=Global_active_power, ylab="Global Active Power (kilowatts)", xlab="", type="l"))
+
+dev.off()
